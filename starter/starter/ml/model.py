63c93a3c9017fdc65099efd1522bd0e1dc466df1
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-15 01:23:47 -0300

    Add Script to train and test a model

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,7 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+import pandas as pd
+from data import process_data
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +19,12 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    model = RandomForestClassifier(n_estimatorsint=500, criterion='gini',
+                                           max_depth=100, max_features='auto', random_state=42)
+    model.fit(X_train, y_train)
 
-    pass
+    return model
+
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +54,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : sklearn.ensemble._forest.RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +63,58 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+
+    return preds
+
+
+def compute_model_performance(model, X, categorical_features=[], label=None, encoder=None, lb=None):
+    """ Compute the model performance on slices of the data.
+
+       Inputs
+       ------
+       model : sklearn.ensemble._forest.RandomForestClassifier
+               Trained machine learning model.
+       X : pd.DataFrame
+           Dataframe containing the features and label. Columns in `categorical_features`
+       categorical_features: list[str]
+           List containing the names of the categorical features (default=[])
+       label : str
+           Name of the label column in `X`. If None, then an empty array will be returned
+           for y (default=None)
+       encoder : sklearn.preprocessing._encoders.OneHotEncoder
+           Trained sklearn OneHotEncoder.
+       lb : sklearn.preprocessing._label.LabelBinarizer
+           Trained sklearn LabelBinarizer.
+
+       Returns
+       -------
+       performance_slices : pd.DataFrame
+           Dataframe with precisiom, recall and f1 in every possible slice of the X dataframe
+           based on the categorical features.
+       """
+
+    model_peformance = {}
+    # loop for all categorical_features
+    for feature in categorical_features:
+        feature_slices = X[feature].unique().tolist()
+        # create a slice for every unique value under the feature
+        for f_slice in feature_slices:
+            # get the slice
+            df_slice = X[X[feature] == f_slice]
+            # process data from the slice
+            X_test_slice, y_test_slice, _, _ = process_data(
+                df_slice, categorical_features=categorical_features,
+                label=label, training=False, encoder=encoder, lb=lb)
+            # get inference from the slice
+            preds = inference(model, X_test_slice)
+            # save the results in a dict
+            model_peformance[f'{feature}_{f_slice}'] = compute_model_metrics(y_test_slice, preds)
+
+    # build a dataframe with the results
+    performance_slices = pd.DataFrame(model_peformance).T
+    performance_slices.columns = ['precision', 'recall', 'f1']
+    performance_slices['slice'] = performance_slices.index
+    performance_slices.reset_index(inplace=True, drop=True)
+
+    return performance_slices
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,10 +3,17 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.model import compute_model_metrics, inference, compute_model_performance, train_model
+from ml.data import process_data
+import pandas as pd
+import logging
+import joblib
 
 # Add code to load in the data.
-
+logging.info("Reading the Data...")
+data = pd.read_csv('../data/census_clean.csv')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
+logging.info("Splitting the Data into train a test sets...")
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +26,34 @@ cat_features = [
     "sex",
     "native-country",
 ]
+logging.info("processing the data...")
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 # Train and save a model.
+logging.info("Training a Random Forest Classifier")
+model = train_model(X_train, y_train)
+
+logging.info("Saving the Model")
+joblib.dump(model, '../model/rfc_model.pkl')
+
+logging.info("Computing the model performance")
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+performance_slices = compute_model_performance(model, X_test, categorical_features=cat_features,
+                          label="salary", encoder=encoder, lb=lb)
+
+performance_slices = performance_slices.append({'f1': fbeta,
+                                                'precision': precision,
+                                                'recall': recall,
+                                                'slice': 'all_data'},
+                                                 ignore_index=True)
+logging.info("Saving Model Performance...")
+
+performance_slices.to_csv('../model/model_performance.csv')
