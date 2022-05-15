f168c942cdc9d5286c58d2eb26f1608b30f6ea02
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-15 20:20:23 -0300

    Decrease number samples in Tests

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -14,7 +14,7 @@ cat_features = [
     "sex",
     "native-country",
 ]
-data = pd.read_csv('starter/data/census_clean.csv')
+data = pd.read_csv('starter/data/census_clean.csv').iloc[:50]
 
 X, y, encoder, lb = process_data(
         data, categorical_features=cat_features, label="salary", training=True
