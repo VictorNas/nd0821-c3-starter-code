7ff4b8e02a6d0c8d7b4015034a115a36b1e04433
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-15 19:07:27 -0300

    Testing

--- starter/starter/ml/test_data.py
+++ starter/starter/ml/test_data.py
@@ -13,7 +13,7 @@ cat_features = [
 ]
 
 def test_process_data():
-    data = pd.read_csv('./starter/data/census_clean.csv')
+    data = pd.read_csv('starter/data/census_clean.csv')
     X, y, encoder, lb = process_data(
         data, categorical_features=cat_features, label="salary", training=True
     )
