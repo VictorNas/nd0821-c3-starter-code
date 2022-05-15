1d7b23391d3405c07b22e094f352186ebe26c5a8
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-15 19:26:07 -0300

    Ass Dvc Pull

--- starter/main.py
+++ starter/main.py
@@ -5,13 +5,8 @@ import pandas as pd
 from starter.ml.model import load_model
 from predict import predict
 import os
-import logging
 
 # load the model
-print(os.listdir('./'))
-print(os.listdir('./starter'))
-print(os.listdir('./starter/model'))
-logging.info(str(os.listdir('./starter/model')))
 model, encoder, lb = load_model('./starter/model')
 
 # Declare the data object with its components and their type.
