8189ca754898877a483f2f58e2a90b9c7395abd0
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-15 21:14:17 -0300

    Add integration with heroku

--- starter/main.py
+++ starter/main.py
@@ -6,6 +6,11 @@ from starter.ml.model import load_model
 from predict import predict
 import os
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 # load the model
 model, encoder, lb = load_model('./starter/model')
 
