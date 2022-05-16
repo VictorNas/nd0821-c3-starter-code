0483e1dc85687b020829183e366e22765c65c407
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-16 00:00:21 -0300

    Add files to root

---
+++ main.py
@@ -0,0 +1,53 @@
+# Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from starter.ml.model import load_model
+from predict import predict
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+# load the model
+model, encoder, lb = load_model('./starter/starter/model')
+
+# Declare the data object with its components and their type.
+def alias_function(x : str) -> str:
+    return x.replace('_','-')
+
+class Input_Data(BaseModel):
+    age: int = 38
+    workclass: str = "Private"
+    fnlgt: int = 28887
+    education: str = "11th"
+    education_num: int = 7
+    marital_status: str = "Married-civ-spouse"
+    occupation: str = "Sales"
+    relationship: str = "Husband"
+    race: str = "White"
+    sex: str = 'Male'
+    capital_gain: int = 0
+    capital_loss: int = 0
+    hours_per_week: int = 50
+    native_country: str = "United-States"
+    class Config:
+        alias_generator = alias_function
+
+# Instantiate the app.
+app = FastAPI()
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def greetings():
+    return {"greeting": "Welcome to Incomer Predictor API!!"}
+
+@app.post("/predict/")
+async def predict_item(item: Input_Data):
+
+    data = pd.DataFrame(item.dict(by_alias=True),index=[0])
+    preds = predict(data,model,encoder,lb)
+    preds = ['<=50K' if p ==0 else '>50K' for p in preds]
+    return {'preds': preds}
---
+++ predict.py
@@ -0,0 +1,20 @@
+from starter.ml.data import process_data
+from starter.ml.model import inference
+
+def predict(item ,model, encoder, lb ):
+    cat_features = [ "workclass",
+                     "education",
+                     "marital-status",
+                     "occupation",
+                     "relationship",
+                     "race",
+                     "sex",
+                     "native-country"]
+
+    X, _ , _, _ = process_data(item, categorical_features= cat_features,
+                               label=None, training=False, encoder=encoder, lb=lb
+    )
+
+    preds = inference(model, X)
+    print('Preds', preds)
+    return preds
